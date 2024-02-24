b86ae55a84d75973bc3aa27b72547d083b7e359e
Fenilpanseriya <fenilpanseriya2004@gmail.com>
2024-02-24 22:09:06 +0530

    change url for backend request

--- src/Axios.js
+++ src/Axios.js
@@ -4,6 +4,6 @@ import axios from "axios"
 
 console.log(process.env.BASE_URL);
 export const Axios=axios.create({
-    baseURL: "https://learnyard121.onrender.com" || "http://localhost:6060/api/v1",
+    baseURL: "https://learnyard121.onrender.com/api/v1" || "http://localhost:6060/api/v1",
     withCredentials:true
 })
